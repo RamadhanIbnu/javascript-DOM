35161d8f3eded1c55bc5e875d3b691f5a078ea21
RamadhanIbnu <ramadhanibnu77@gmail.com>
2020-08-19 13:04:26 +0700

    adding courses from slide show by Impact Byte

---
+++ practice2/impactByteCourse/script.js
@@ -0,0 +1,7 @@
+let tittle = document.getElementById('title');
+tittle.addEventListener('mouseover', displayAlert)
+tittle.style.display = "inline" ;
+
+function displayAlert(){
+    alert("eaaa mau ngapain?");
+}
